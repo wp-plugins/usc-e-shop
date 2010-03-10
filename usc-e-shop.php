d8611d2cb1312d04905c94684749d7e24ab3b033
uscnanbu <uscnanbu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-10 01:01:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/usc-e-shop/trunk@215549 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- usc-e-shop.php
+++ usc-e-shop.php
@@ -3,12 +3,12 @@
 Plugin Name: Welcart e-Commerce
 Plugin URI: http://www.usconsort.com/usces/
 Description: Welcart builds the management system with a net shop on Wordpress.
-Version: 0.3
+Version: 0.4
 Author: USconsort
 Author URI: http://www.uscons.co.jp/
 */
 
-define('USCES_VERSION', '0.3');
+define('USCES_VERSION', '0.4');
 define('USCES_DB_ACCESS', '1.1');
 define('USCES_DB_MEMBER', '1.1');
 define('USCES_DB_ORDER', '1.7');
