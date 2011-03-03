d0a904e3f188805d75cc477fb3ea049a562b8129
uscnanbu <uscnanbu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-03 07:15:33 +0000

    0000102 nanbu
    git-svn-id: https://plugins.svn.wordpress.org/usc-e-shop/trunk@354382 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- usc-e-shop.php
+++ usc-e-shop.php
@@ -7,7 +7,7 @@ Version: 0.9.3
 Author: USconsort
 Author URI: http://www.uscons.co.jp/
 */
-define('USCES_VERSION', '0.9.3.r1102261');
+define('USCES_VERSION', '0.9.3.r1103031');
 define('USCES_DB_ACCESS', '1.4');
 define('USCES_DB_MEMBER', '1.1');
 define('USCES_DB_MEMBER_META', '1.1');
