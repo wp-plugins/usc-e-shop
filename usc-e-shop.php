4f9e25f56bec12f7e6fe312b10d7a4823cd6e375
uscnanbu <uscnanbu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-22 03:29:00 +0000

    0000184,0000185 nanbu
    git-svn-id: https://plugins.svn.wordpress.org/usc-e-shop/trunk@399674 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- usc-e-shop.php
+++ usc-e-shop.php
@@ -7,7 +7,7 @@ Version: 1.0.3
 Author: USconsort
 Author URI: http://www.uscons.co.jp/
 */
-define('USCES_VERSION', '1.0.3.r1106141');
+define('USCES_VERSION', '1.0.3.r1106221');
 define('USCES_DB_ACCESS', '1.4');
 define('USCES_DB_MEMBER', '1.1');
 define('USCES_DB_MEMBER_META', '1.1');
@@ -28,7 +28,7 @@ define('USCES_MEMBER_FOLDER', 'usces-member');
 define('USCES_ADMIN_SSL_BASE_NAME', 'admin-ssl.php');
 define('USCES_ADMIN_URL', get_option('siteurl') . '/wp-admin/admin.php');
 
-global $usces_settings, $usces_states;
+global $usces_settings, $usces_states, $usces_p;
 require_once(USCES_PLUGIN_DIR."/functions/included_first.php");
 add_filter( 'locale', 'usces_filter_locale' );
 load_plugin_textdomain('usces', USCES_PLUGIN_DIR.'/languages', USCES_PLUGIN_FOLDER.'/languages');
