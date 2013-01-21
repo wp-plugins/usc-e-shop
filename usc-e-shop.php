02ba4e4492fe75817b2be8b3d89ce949bf60e07b
uscnanbu <uscnanbu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-21 07:58:05 +0000

    0000628 20130121ysk 2
    git-svn-id: https://plugins.svn.wordpress.org/usc-e-shop/trunk@656129 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- usc-e-shop.php
+++ usc-e-shop.php
@@ -7,7 +7,7 @@ Version: 1.2.2
 Author: Collne inc.
 Author URI: http://www.welcart.com/
 */
-define('USCES_VERSION', '1.2.2.1301211');
+define('USCES_VERSION', '1.2.2.1301212');
 define('USCES_DB_ACCESS', '1.5');
 define('USCES_DB_MEMBER', '1.1');
 define('USCES_DB_MEMBER_META', '1.1');
