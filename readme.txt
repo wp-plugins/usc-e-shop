87bd4f084c72d346a56c4e3ededef509a55064cd
uscnanbu <uscnanbu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-01 05:39:11 +0000

    1.3.1 nanbu
    git-svn-id: https://plugins.svn.wordpress.org/usc-e-shop/trunk@689822 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: USconsort
 Tags: Welcart, e-Commerce, shopping, cart, eShop, store, admin, calendar, manage, plugin, shortcode, widgets, membership
 Requires at least: 3.2
 Tested up to: 3.5
-Stable tag: 1.3
+Stable tag: 1.3.1
 
 Welcart assists you to build online shop system.
 It is developed in Japan.
--- usc-e-shop.php
+++ usc-e-shop.php
@@ -3,11 +3,11 @@
 Plugin Name: Welcart e-Commerce
 Plugin URI: http://www.welcart.com/
 Description: Welcart builds the management system with a net shop on Wordpress.
-Version: 1.3
+Version: 1.3.1
 Author: Collne inc.
 Author URI: http://www.welcart.com/
 */
-define('USCES_VERSION', '1.3.0.1304011');
+define('USCES_VERSION', '1.3.1.1304012');
 define('USCES_DB_ACCESS', '1.5');
 define('USCES_DB_MEMBER', '1.1');
 define('USCES_DB_MEMBER_META', '1.1');
