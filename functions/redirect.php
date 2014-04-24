4625563d62fd67c2acc9f03fd0dec245fe33c618
uscnanbu <uscnanbu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-24 00:21:47 +0000

    0000873 kitamura
    git-svn-id: https://plugins.svn.wordpress.org/usc-e-shop/trunk@901332 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- functions/redirect.php
+++ functions/redirect.php
@@ -1,22 +1,23 @@
 <?php
 function usces_redirect(){
-	
-	if ( isset($_POST['wp-preview']) && 'dopreview' == $_POST['wp-preview'] ){
-		$action = 'preview';
-	}else{
-		$action = '';
-	}
-	
-	switch( $action ){
-		case 'preview':
-			check_admin_referer( 'autosave', 'autosavenonce' );
+	if( isset($_GET['page']) && $_GET['page'] == 'usces_itemedit' ){	
+		if ( isset($_POST['wp-preview']) && 'dopreview' == $_POST['wp-preview'] ){
+			$action = 'preview';
+		}else{
+			$action = '';
+		}
 		
-			$url = post_preview();
-		
-			wp_redirect($url);
-			exit();
-			break;
+		switch( $action ){
+			case 'preview':
+				check_admin_referer( 'autosave', 'autosavenonce' );
 			
+				$url = post_preview();
+			
+				wp_redirect($url);
+				exit();
+				break;
+				
+		}
 	}
 }
 ?>
