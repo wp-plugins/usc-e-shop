566fa6872a4bb6c8bd4cb14f34f0d0c02858f06f
uscnanbu <uscnanbu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-08 00:26:55 +0000

    0000960 20140708ysk
    git-svn-id: https://plugins.svn.wordpress.org/usc-e-shop/trunk@944614 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/purchase_button.php
+++ includes/purchase_button.php
@@ -10,11 +10,11 @@ $cart = $usces->cart->get_cart();
 //$purchase_disabled = ( '' != $usces->error_message ) ? ' disabled="true"' : '';
 $purchase_disabled = '';
 
-if( 'acting' != substr($payments['settlement'], 0, 6)  || 0 == $usces_entries['order']['total_full_price'] ){
-	$html .= '<form id="purchase_form" action="' . USCES_CART_URL . '" method="post" onKeyDown="if (event.keyCode == 13) {return false;}">
+if( 'acting' != substr($payments['settlement'], 0, 6) || 0 == $usces_entries['order']['total_full_price'] ){
+	$purchase_html = '<form id="purchase_form" action="' . USCES_CART_URL . '" method="post" onKeyDown="if (event.keyCode == 13) {return false;}">
 		<div class="send"><input name="backDelivery" type="submit" id="back_button" class="back_to_delivery_button" value="'.apply_filters('usces_filter_confirm_prebutton_value', __('Back to payment method page.', 'usces')).'"' . apply_filters('usces_filter_confirm_prebutton', NULL) . ' />
 		<input name="purchase" type="submit" id="purchase_button" class="checkout_button" value="'.__('Checkout', 'usces').'"' . apply_filters('usces_filter_confirm_nextbutton', NULL) . $purchase_disabled . ' /></div>';
-	$html = apply_filters('usces_filter_confirm_inform', $html, $payments, $acting_flag, $rand, $purchase_disabled);
+	$html = apply_filters('usces_filter_confirm_inform', $purchase_html, $payments, $acting_flag, $rand, $purchase_disabled);
 	$html .= '</form>';
 }else{
 	$send_item_code = apply_filters('usces_filter_settlement_item_code', $usces->getItemCode($cart[0]['post_id']));
